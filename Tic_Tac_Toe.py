e7e2e2237f7f779c049bc6f7d7c47efef343f6d0
Miguel López <lop18028@byui.edu>
2022-01-12 11:37:44 -0700

    Add create_board and display board functions.
    Add create_board function --> This function just create a board.
    Add display_board function --> Displays in terminal the board.

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -6,4 +6,33 @@ Author: Miguel López.
 
 # Functions.
 def main():
-    pass
+    # Create board.
+    board = create_board()
+
+    print ( board )
+
+def create_board():
+    """Create a list called board.
+    Return: Board.
+    """
+    board = []
+    for square in range(9):
+        board.append(square + 1)
+    return board
+
+def display_board(board):
+    """Display the board in terminal.
+    Parameters
+        Board: The board to be displayed.
+    Return: Nothing.
+    """
+    print()
+    print(f"{board[0]}|{board[1]}|{board[2]}")
+    print('-+-+-')
+    print(f"{board[3]}|{board[4]}|{board[5]}")
+    print('-+-+-')
+    print(f"{board[6]}|{board[7]}|{board[8]}")
+    print()
+
+if __name__ == "__main__":
+    main()
